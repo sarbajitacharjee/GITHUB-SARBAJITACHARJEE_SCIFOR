9b49bc5b81f7b868200a96604cf83be130becbca
Sarbajit Acharjee <87385430+sarbajitacharjee@users.noreply.github.com>
2024-09-12 19:48:23 +0530

    deleted

--- Assignment/Javascript/max.js
+++
@@ -1,8 +0,0 @@
-function maxx(a, b, c) {
-    let maxNumber = a; 
-    if (c > maxNumber) maxNumber = c;
-    if (b > maxNumber) maxNumber = b; 
-     return maxNumber;
-}
-console.log(" max number : ")
-console.log(maxx(110, 120, 115));
